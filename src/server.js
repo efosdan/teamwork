acf602c84fbd5c5c7cb7de002d6314903efc9868
Efosa Daniel <edfocmetro@gmail.com>
2023-10-15 07:26:06 +0200

    edited port config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const client = require("./db.js")
 const express = require('express');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log(`Team server listening at port ${port}!`)
